a056bd3e05b9aef51630eb245fee707c0bef03c3
saugatadk <adhikarisaugat55@gmail.com>
2025-04-27 22:35:32 +0545

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+number1 = input("enter firstt number: ");
+number2 = input("enter second number: ");
+
+sum = int(number1) + int(number2);
+print("sum of two numbers is:", sum);
+print("hello world");
