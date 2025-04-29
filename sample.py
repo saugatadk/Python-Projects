c0f35e42a9d4b08709e48b99b89c30cffba89343
saugatadk <adhikarisaugat55@gmail.com>
2025-04-29 20:21:37 +0545

    Sample program

---
+++ sample.py
@@ -0,0 +1,17 @@
+#wap to add only positive numbers from user if negative skip and if zero exit the loop
+
+sum = 0;
+while True:
+    num = int(input("Enter a number: "));
+    
+    if num < 0:
+        print("Negative number entered. Enter a positive number");
+        continue
+    elif num == 0:
+        print("Zero entered. Exiting the program");
+        break;
+    else:
+        sum += num;
+        print("Current sum:", sum);
+print("Final sum of positive numbers:", sum);
+        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,3 @@
-number1 = input("enter firstt number: ");
-number2 = input("enter second number: ");
 
 sum = int(number1) + int(number2);
 print("sum of two numbers is:", sum);
