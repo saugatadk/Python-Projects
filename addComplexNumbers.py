ba8158f0011b9d0fd6a23787fe0b04691dc7a927
saugatadk <adhikarisaugat55@gmail.com>
2025-05-03 14:42:54 +0545

    Add 2 complex Numbers using OOP

---
+++ addComplexNumbers.py
@@ -0,0 +1,17 @@
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imaginary = imag
+    
+    def add(self, number):
+        result_real = self.real + number.real
+        result_imaginary = self.imaginary + number.imaginary
+        result = Complex(result_real, result_imaginary)
+        return result
+
+n1 = Complex(1, 9)
+n2 = Complex(2, 10)
+
+n3 = n1.add(n2)
+print(f"Real:", n3.real)
+print(f"Imaginary:", n3.imaginary)        
