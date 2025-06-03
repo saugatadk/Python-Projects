29c714a9c683b12790b1634d20dba2159129d97a
saugatadk <adhikarisaugat55@gmail.com>
2025-06-03 18:05:09 +0545

    Add Vectorization time calculation

---
+++ ML/vectorization.py
@@ -0,0 +1,31 @@
+import numpy as np
+import time
+
+def my_dot(a, b):
+    """Compute dot product of two vectors using a loop."""
+    if len(a) != len(b):
+        raise ValueError("Vectors must be the same length")
+    result = 0.0
+    for i in range(len(a)):
+        result += a[i] * b[i]
+    return result
+
+np.random.seed(1)
+a = np.random.rand(10000000)  # very large arrays
+b = np.random.rand(10000000)
+
+tic = time.time()  # capture start time
+c = np.dot(a, b)
+toc = time.time()  # capture end time
+
+print(f"np.dot(a, b) =  {c:.4f}")
+print(f"Vectorized version duration: {1000*(toc-tic):.4f} ms ")
+
+tic = time.time()  # capture start time
+c = my_dot(a,b)
+toc = time.time()  # capture end time
+
+print(f"my_dot(a, b) =  {c:.4f}")
+print(f"loop version duration: {1000*(toc-tic):.4f} ms ")
+
+del(a);del(b)  #remove these big arrays from memory
